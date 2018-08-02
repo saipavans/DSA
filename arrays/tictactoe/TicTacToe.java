fc77a22f5f97992633e34a57a68a7493e70ea8b8
saipavans <sp403250@dal.ca>
2018-08-02 12:58:16 -0300

    Implemented initial conditions

---
+++ arrays/tictactoe/TicTacToe.java
@@ -0,0 +1,15 @@
+package arrays.tictactoe;
+
+public class TicTacToe {
+	
+	private static final int X = 1, O = -1, EMPTY = 0;
+	private int board[][] = null;
+	private int player = X; // X starts first
+	
+	public TicTacToe(){
+		board = new int[3][3];
+	}
+	
+	
+
+}
