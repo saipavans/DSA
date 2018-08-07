4fb0574b37015b4ce837784696c5310cf2216f3c
saipavans <sp403250@dal.ca>
2018-08-07 15:21:10 -0300

    Implemented Circular Lists with Singly Node

---
+++ lists/CircularlyLinkedList.java
@@ -0,0 +1,67 @@
+package lists;
+
+public class CircularlyLinkedList<E> {
+	
+	private SinglyNode<E> tail = null; // NOTE: Head in this case is implicitly next of tail node
+	private int size = 0;
+	
+	// Returns the number of elements in the list
+	public int size() {
+		return size;
+	}
+	
+	// Returns true if the list is empty
+	public boolean isEmpty(){
+		return size == 0;
+	}
+	
+	// Returns the first element without removing it.
+	public E first(){
+		return size == 0 ? null : tail.getNext().getElement();
+	}
+	
+	// Returns the last element without removing it.
+	public E last(){
+		return size == 0 ? null : tail.getElement();
+	}
+	
+	
+	// Adds a new element at the start of the list
+	public void addFirst(E element){
+		SinglyNode<E> newElement = new SinglyNode<E>(element, null);
+		if (size == 0){ // List was empty before adding
+			tail = newElement;
+			tail.setNext(tail);
+		} else{
+			newElement.setNext(tail.getNext());
+			tail.setNext(newElement);
+		}
+		size += 1;
+	}
+	
+	// Adds a new element at the end of the list
+	public void addLast(E element){
+		addFirst(element);
+		if (size > 1){ // List was non empty before adding
+			tail = tail.getNext();
+		}
+	}
+	
+	// Removes a new element at the start of the list and returns it.
+	public E removeFirst(){
+		if (size == 0){
+			return null;
+		}
+		E elementRemoved = first();
+		tail.setNext(tail.getNext().getNext());
+		size -= 1;
+		return elementRemoved;
+	}
+	
+	// Rotate the first node to the back of the list
+	public void rotate(){
+		if (size > 0){ // Note: When size is 1, nothing happens
+			tail = tail.getNext();
+		}
+	}
+}
--- lists/TestSimulation.java
+++ lists/TestSimulation.java
@@ -6,7 +6,9 @@ public class TestSimulation {
 	
 	public static void main(String[] args) {
 		
-		SinglyLinkedList singlyList = new SinglyLinkedList();
+		// Testing Singly Linked List
+		
+		SinglyLinkedList<Integer> singlyList = new SinglyLinkedList<Integer>();
 		System.out.println("Size of Singly Linked List: " + singlyList.size());
 		
 		singlyList.addFirst(1);
@@ -25,6 +27,30 @@ public class TestSimulation {
 		removedInt = (int) singlyList.removeFirst();
 		System.out.println("The following element was removed: " + removedInt);
 		System.out.println("Size of Singly Linked List: " + singlyList.size());
+		
+		
+		
+		// Testing Circularly Linked List
+		
+		CircularlyLinkedList<Integer> circularList = new CircularlyLinkedList<Integer>();
+		System.out.println("Size of Circular Linked List: " + circularList.size());
+		
+		circularList.addFirst(4);
+		circularList.addFirst(3);
+		circularList.addFirst(2);
+		circularList.addFirst(1);
+		circularList.addLast(5);
+		System.out.println("Size of Circular Linked List: " + circularList.size());
+		System.out.println("First Element: " + circularList.first());
+		System.out.println("Last Element: " + circularList.last());
+		System.out.println("Rotating ..."); 
+		circularList.rotate();
+		System.out.println("First Element: " + circularList.first());
+		System.out.println("Last Element: " + circularList.last());
+		System.out.println("Removing First Element: " + circularList.removeFirst());
+		System.out.println("Size of Circular Linked List: " + circularList.size());		
+		System.out.println("First Element: " + circularList.first());
+		System.out.println("Last Element: " + circularList.last());
 	
 	}
 
