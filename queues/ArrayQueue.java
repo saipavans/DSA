a19a9798ab7b3f46857f211c8c56434e1c8b39d0
saipavans <sp403250@dal.ca>
2018-08-16 02:37:40 -0300

    Queue implemented using array (circular array)

---
+++ queues/ArrayQueue.java
@@ -0,0 +1,59 @@
+package queues;
+
+public class ArrayQueue<E> implements Queue<E> {
+	
+	private static final int DEFAULT_CAPACITY = 1000;
+	
+	private E[] data = null;
+	private int size = 0;
+	private int front = 0; // Dequeue at front and Enqueue at the rear
+	
+	
+	public ArrayQueue(int capacity) {
+		data = (E[]) new Object[capacity];
+	}
+
+	public ArrayQueue() {
+		this(DEFAULT_CAPACITY);
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void enqueue(E element) {
+		
+		if (size == data.length) {
+			throw new IllegalStateException("Queue is full");
+		}
+		
+		int rear = (front + size) % data.length; // Modulo the capacity to make the array circular
+		data[rear] = element;
+		size++;
+	}
+	
+	public E first() {
+		if (isEmpty()) {
+			return null;
+		}
+		return data[front];
+	}
+	
+	public E dequeue() {
+		if (isEmpty()) {
+			return null;
+		}
+		
+		E removedElement = data[front];
+		data[front] = null; // Making it eligible for GC
+		front = (front + 1) % data.length;
+		size--;
+		return removedElement;
+	}
+	
+	
+}
---
+++ queues/TestSimulation.java
@@ -0,0 +1,27 @@
+package queues;
+
+public class TestSimulation {
+	
+	private static Queue<Integer> numbers = new ArrayQueue<Integer>();
+
+	public static void main(String[] args) {
+		
+		System.out.println("Initially, queue empty ?: " + numbers.isEmpty());
+		
+		numbers.enqueue(87);
+		numbers.enqueue(85);
+		numbers.enqueue(97);
+		numbers.dequeue();
+		numbers.enqueue(87);
+		numbers.enqueue(65);
+		numbers.enqueue(67);
+		
+		
+		System.out.println("Dequeueing elements one by one ...");
+		while (!numbers.isEmpty()) {
+			System.out.println (numbers.dequeue());
+		}
+		
+	}
+
+}
